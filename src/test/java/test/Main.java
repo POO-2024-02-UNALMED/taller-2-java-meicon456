c80968bdb0b9063dcfde474de46ecf0f57ba0912
meicon456 <jocastrore@unal.edu.co>
2024-11-12 10:33:40 -0500

    Update Main.java

--- src/test/java/test/Main.java
+++ src/test/java/test/Main.java
@@ -1,95 +1,167 @@
 package test;
 
-class Motor {
-  private int numCilindros;
-  private String tipo;
-  private int registro;
+public class Motor {
+    private int numeroCilindros;
+    private String tipo;
+    private int registro;
 
-  public Motor(int numeroCilindros, String tipo, int registro) {
-      this.numCilindros = numeroCilindros;
-      this.tipo = tipo;
-      this.registro = registro;
-  }
+    public Motor(int numeroCilindros, String tipo, int registro) {
+        this.numeroCilindros = numeroCilindros;
+        this.tipo = tipo;
+        this.registro = registro;
+    }
 
-  public void cambiarRegistro(int registro) {
-      this.registro = registro;
-  }
+    public int getNumeroCilindros() {
+        return numeroCilindros;
+    }
 
-  public void asignarTipo(String tipo) {
-      if (tipo.equals("gasolina") || tipo.equals("electrico")) {
-          this.tipo = tipo;
-      }
-  }
+    public void setNumeroCilindros(int numeroCilindros) {
+        this.numeroCilindros = numeroCilindros;
+    }
 
-  public int getRegistro() {
-      return registro;
-  }
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(int registro) {
+        this.registro = registro;
+    }
+
+    public void cambiarRegistro(int nuevoRegistro) {
+        this.registro = nuevoRegistro;
+    }
+
+    public void asignarTipo(String nuevoTipo) {
+        this.tipo = nuevoTipo;
+    }
 }
 
-class Asiento {
-  private String color;
-  private int precio;
-  private int registro;
+public class Asiento {
+    private String color;
+    private int precio;
+    private int registro;
 
-  public Asiento(String color, int precio, int registro) {
-      this.color = color;
-      this.precio = precio;
-      this.registro = registro;
-  }
+    public Asiento(String color, int precio, int registro) {
+        this.color = color;
+        this.precio = precio;
+        this.registro = registro;
+    }
 
-  public void cambiarColor(String color) {
-      if (color.equals("rojo") || color.equals("amarillo") || color.equals("verde") ||
-          color.equals("negro") || color.equals("blanco")) {
-          this.color = color;
-      }
-  }
+    public String getColor() {
+        return color;
+    }
 
-  public int getRegistro() {
-      return registro;
-  }
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(int registro) {
+        this.registro = registro;
+    }
+
+    public void cambiarColor(String nuevoColor) {
+        this.color = nuevoColor;
+    }
 }
 
-class Auto {
-  public static int cantidadCreados = 0;
-  private String marca;
-  private String modelo;
-  private int precio;
-  private Asiento[] asientos;
-  private Motor motor;
-  private int registro;
+public class Auto {
+    private String marca;
+    private String modelo;
+    private int precio;
+    private Asiento[] asientos;
+    private Motor motor;
+    private int registro;
 
-  public Auto(String marca, String modelo, int precio, Object[][] asientosData, Motor motor, int registro) {
-      this.marca = marca;
-      this.modelo = modelo;
-      this.precio = precio;
-      this.motor = motor;
-      this.registro = registro;
+    public Auto(String marca, String modelo, int precio, Asiento[] asientos, Motor motor, int registro) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+        this.asientos = asientos;
+        this.motor = motor;
+        this.registro = registro;
+    }
 
-      this.asientos = new Asiento[asientosData.length];
-      for (int i = 0; i < asientosData.length; i++) {
-          String color = (String) asientosData[i][0];
-        int asientoPrecio = Integer.parseInt((String) asientosData[i][1]);          
-          this.asientos[i] = new Asiento(color, asientoPrecio, registro);
-      }
+    public int cantidadAsientos() {
+        int count = 0;
+        for (Asiento asiento : asientos) {
+            if (asiento != null) count++;
+        }
+        return count;
+    }
 
-      cantidadCreados++;
-  }
+    public String verificarIntegridad() {
+        if (motor.getRegistro() != registro) return "Las piezas no son originales";
+        for (Asiento asiento : asientos) {
+            if (asiento != null && asiento.getRegistro() != registro) return "Las piezas no son originales";
+        }
+        return "Auto original";
+    }
 
-  public int cantidadAsientos() {
-      return asientos.length;
-  }
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
 
-  public String verificarIntegridad() {
-      if (motor.getRegistro() != registro) {
-          return "Las piezas no son originales";
-      }
+    public String getMarca() {
+        return marca;
+    }
 
-      for (Asiento asiento : asientos) {
-          if (asiento.getRegistro() != registro) {
-              return "Las piezas no son originales";
-          }
-      }
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
 
-      return "Auto original";
-  }
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setAsientos(Asiento[] asientos) {
+        this.asientos = asientos;
+    }
+
+    public Asiento[] getAsientos() {
+        return asientos;
+    }
+
+    public void setMotor(Motor motor) {
+        this.motor = motor;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public void setRegistro(int registro) {
+        this.registro = registro;
+    }
+
+    public int getRegistro() {
+        return registro;
+    }
 }
