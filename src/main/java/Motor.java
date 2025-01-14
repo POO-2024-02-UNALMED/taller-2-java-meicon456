985fc2254485897d70b6f470d4d4b6dc3529834f
meicon456 <jocastrore@unal.edu.co>
2025-01-14 10:49:23 -0500

    Update and rename Borrar.txt to Motor.java

---
+++ src/main/java/Motor.java
@@ -0,0 +1,45 @@
+package test;
+
+public class Motor {
+    private int numeroCilindros;
+    private String tipo;
+    private int registro;
+
+    public Motor(int numeroCilindros, String tipo, int registro) {
+        this.numeroCilindros = numeroCilindros;
+        this.tipo = tipo;
+        this.registro = registro;
+    }
+
+    public int getNumeroCilindros() {
+        return numeroCilindros;
+    }
+
+    public void setNumeroCilindros(int numeroCilindros) {
+        this.numeroCilindros = numeroCilindros;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getRegistro() {
+        return registro;
+    }
+
+    public void setRegistro(int registro) {
+        this.registro = registro;
+    }
+
+    public void cambiarRegistro(int nuevoRegistro) {
+        this.registro = nuevoRegistro;
+    }
+
+    public void asignarTipo(String nuevoTipo) {
+        this.tipo = nuevoTipo;
+    }
+}
